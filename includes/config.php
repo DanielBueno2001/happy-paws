5ce0f153ee3ac187984baaa945fe9dc67c8a5ab1
DanielBueno2001 <est.daniel.bueno@unimilitar.edu.co>
2025-04-07 15:17:50 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,9 @@
 <?php
 // Credenciales RDS
-define('DB_HOST', 'happy-paws-db.c7ea6ego67h8.us-east-2.rds.amazonaws.com'); // Reemplaza con tu endpoint
+define('DB_HOST', 'happypaws.c7ea6ego67h8.us-east-2.rds.amazonaws.com'); // Reemplaza con tu endpoint
 define('DB_USER', 'admin');
 define('DB_PASS', 'Simon2025_');
-define('DB_NAME', 'happy-paws-db');
+define('DB_NAME', 'happypaws');
 
 // Evitar acceso directo
 defined('BASE_PATH') or die('Acceso denegado');
