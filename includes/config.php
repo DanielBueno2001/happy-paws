1f25278784d0b68e3c0f6b823a56c2d0e4d19142
DanielBueno2001 <est.daniel.bueno@unimilitar.edu.co>
2025-04-07 14:27:18 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'happy-paws-db.c7ea6ego67h8.us-east-2.rds.amazonaws.com'); // Reemplaza con tu endpoint
 define('DB_USER', 'admin');
 define('DB_PASS', 'Simon2025_');
-define('DB_NAME', 'happy_paws_db');
+define('DB_NAME', 'happy-paws-db');
 
 // Evitar acceso directo
 defined('BASE_PATH') or die('Acceso denegado');
